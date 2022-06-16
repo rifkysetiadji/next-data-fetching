7fec9690ed881cb023b2fe32392e2b9938f43c1d
rifkysetiadji <rifkywikrama.s@gmail.com>
2022-06-16 16:10:31 +0700

    add isr

---
+++ pages/isr.js
@@ -0,0 +1,26 @@
+import React from "react";
+import base64 from "base-64";
+import { loadPosts } from "../lib/fetch-posts";
+export default function Ssr({ posts }) {
+  return (
+    <div>
+      {posts.map((d, i) => (
+        <p key={i}>{d.webEvent.title}</p>
+      ))}
+    </div>
+  );
+}
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const posts = await loadPosts();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  };
+}
